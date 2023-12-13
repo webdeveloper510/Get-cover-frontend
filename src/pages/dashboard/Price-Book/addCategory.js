5cb791c622e4885d688ef78cb68ae5c191cc5122
developer <developerweb6@gmail.com>
2023-12-13 20:36:53 +0530

    changes

--- src/pages/dashboard/Price-Book/addCategory.js
+++ src/pages/dashboard/Price-Book/addCategory.js
@@ -1,25 +1,65 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import Headbar from '../../../common/headBar'
 import Grid from '../../../common/grid';
 import Input from '../../../common/input';
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 // Media Include
 import BackImage from '../../../assets/images/icons/backArrow.svg'
 import Button from '../../../common/button';
 import Modal from '../../../common/model';
 import AddDealer from '../../../assets/images/dealer-book.svg'
+import { addCategory } from '../../../services/priceBookService';
 
 function AddCategory() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+  const [error,setError] = useState('')
+  const [timer, setTimer] = useState(5);
+  const navigate = useNavigate();
 
   const closeModal = () => {
     setIsModalOpen(false);
   };
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      description: "",
+      status:true
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Email Required"),
+      description: Yup.string().required("Description Required"),
+    }),
+    onSubmit: async (values) => {
+      console.log("Form values:", values);
+      const result = await addCategory(values);
+      console.log(result);
+      if (result.code !== 200) {
+        setError(result.message);
+      } else {
+         setError(false);
+         setIsModalOpen(true)
+         setTimer(5)
+      }
+    },
+  });
+
+  useEffect(() => {
+    let intervalId;
+    if (isModalOpen && timer > 0) {
+      intervalId = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+    }
+    if (timer === 0) {
+      closeModal();
+      navigate('/category')
+    }
+    return () => clearInterval(intervalId);
+  }, [isModalOpen, timer]);
+
 
   return (
     <div className='my-8 ml-3'>
@@ -36,23 +76,57 @@ function AddCategory() {
           </ul>
         </div>
       </div>
-
+      {error && (
+                  <p className="text-red-500 text-sm pl-2">
+                    <span className="font-semibold"> {} </span>
+                  </p>
+                )}
       {/* Form Start */}
 
-      <form className='mt-8'>
+        <form className='mt-8'  onSubmit={formik.handleSubmit}>
         <div className='px-8 py-8 drop-shadow-4xl bg-white border-[1px] border-[#D1D1D1]  rounded-xl'>
           <Grid>
             <div className='col-span-12'>
-              <Input type='text' name='lName' className="!bg-[#fff]" label='Category Name' required={true} placeholder='Enter' />
+              <Input
+                    type="text"
+                    name="name"
+                    label="Category Name"
+                    placeholder="name"
+                    className="!bg-[#fff]"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+              {formik.touched.name && formik.errors.name && (
+                    <div className="text-red-500 text-sm pl-2 pt-2">
+                      {formik.errors.name}
+                    </div>
+                  )}
             </div>
+  
             <div className='col-span-12'>
-              <div className='relative'>
-                <label for="message" className="absolute text-base text-[#5D6E66] leading-6 duration-300 transform origin-[0] top-1 bg-[#fff] left-2 px-1 -translate-y-4 scale-75">Description <span className="text-red-500">*</span></label>
-                <textarea id="message" rows="4" className=" resize-none block px-2.5 pb-2.5 pt-4 w-full text-base font-semibold text-light-black bg-transparent rounded-lg border-[1px] border-gray-300 appearance-none peer " placeholder="Type here..."></textarea>
-              </div>
-            </div>
+  <div className='relative'>
+    <label htmlFor="description" className="absolute text-base text-[#5D6E66] leading-6 duration-300 transform origin-[0] top-1 bg-[#fff] left-2 px-1 -translate-y-4 scale-75">Description <span className="text-red-500">*</span></label>
+    <textarea
+      id="description"
+      rows="4"
+      name="description"
+      value={formik.values.description}
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      className="resize-none block px-2.5 pb-2.5 pt-4 w-full text-base font-semibold text-light-black bg-transparent rounded-lg border-[1px] border-gray-300 appearance-none peer"
+      placeholder="Type here..."
+    ></textarea>
+    {formik.touched.description && formik.errors.description && (
+      <div className="text-red-500 text-sm pl-2 pt-2">
+        {formik.errors.description}
+      </div>
+    )}
+  </div>
+</div>
+
           </Grid>
-          <Button className='mt-12 font-normal rounded-[25px]' onClick={() => { openModal(true); }}>Submit</Button>
+          <Button type="submit" className='mt-12 font-normal rounded-[25px]' >Submit</Button>
         </div>
       </form>
 
@@ -62,6 +136,10 @@ function AddCategory() {
           <img src={AddDealer} alt='email Image' className='mx-auto' />
           <p className='text-3xl mb-0 mt-4 font-semibold text-neutral-grey'>Submited  <span className='text-light-black'> Successfully </span></p>
           <p className='text-neutral-grey text-base font-medium mt-2'><b> New category </b> added successfully. </p>
+          <p className='text-neutral-grey text-base font-medium mt-2'>
+           Redirecting you on Category Page {timer} seconds.
+          </p>
+
         </div>
 
       </Modal>
---
+++ src/services/priceBookService.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+// Declare the base URL of the API
+const url = process.env.REACT_APP_API_KEY || "fallback_value";
+
+const getAccessToken = () => {
+  const userDetails = JSON.parse(localStorage.getItem('userDetails'));
+  return userDetails ? userDetails.token : null;
+};
+
+const createHeaders = () => {
+  const accessToken = getAccessToken();
+
+  if (accessToken) {
+    return {
+      'x-access-token': accessToken,
+      'Content-Type': 'application/json'
+    };
+  }
+};
+
+// API calls
+export const addCategory = async (categoryDetails) => {
+  const headers = createHeaders();
+
+  try {
+    const response = await axios.post(`${url}/price/createPriceBookCategory`, categoryDetails, { headers });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getCategoryList = async () => {
+    const headers = createHeaders();
+  
+    try {
+      const response = await axios.post(`${url}/price/getPriceBookCategories`,{}, { headers });
+  
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
