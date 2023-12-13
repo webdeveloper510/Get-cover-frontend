e70022a3cead6d904a991ae0267f1144591739c4
Saurav Sharma <developerweb6@gmail.com>
2023-12-13 22:21:51 +0530

    Working on issues

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -13,10 +13,7 @@ function Dashboard() {
         <div className='flex'>
           <div className='pl-3'>
             <p className='font-bold text-[38px] leading-9	mb-[3px]'>Dashboard</p>
-            <ul className='flex self-center'>
-              <li className='text-sm text-neutral-grey font-Regular'><Link to={'/'}>Dashboard </Link>  /  </li>
-              <li className='text-sm text-neutral-grey font-semibold ml-2 pt-[1px]'> Dashboard </li>
-            </ul>
+           
           </div>
         </div>
      </div>
