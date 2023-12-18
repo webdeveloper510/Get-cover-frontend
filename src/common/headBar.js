acd6c9129ed81474f1b28cd72b2c917ea52a5041
neek3190 <neek3190@gmail.com>
2023-12-19 04:57:15 +0530

    changes

--- src/common/headBar.js
+++ src/common/headBar.js
@@ -1,43 +1,53 @@
-import React from 'react'
-import Grid from './grid';
+import React from "react";
+import Grid from "./grid";
 
-// media Import 
-import SearchImage from '../assets/images/icons/search.svg';
-import SettingImage from '../assets/images/icons/Setting.svg';
-import NotificationImage from '../assets/images/icons/Notification.svg';
-import ProfileImage from '../assets/images/icons/Profile.svg';
-import Down from '../assets/images/icons/Drop.svg';
-import { Link } from 'react-router-dom';
+// media Import
+import SearchImage from "../assets/images/icons/search.svg";
+import SettingImage from "../assets/images/icons/Setting.svg";
+import NotificationImage from "../assets/images/icons/Notification.svg";
+import ProfileImage from "../assets/images/icons/Profile.svg";
+import Down from "../assets/images/icons/Drop.svg";
+import { Link, useLocation } from "react-router-dom";
 
-function Headbar({className=''}) {
+function Headbar({ className = "" }) {
+  const location = useLocation();
+  console.log(location.pathname === "/notifications");
   return (
-    <div className=''>
+    <div className="">
       <Grid className={`absolute right-[-1%] top-[5%]  ${className}`}>
-        <div className='col-span-4'></div>
-        <div className='col-span-2'></div>
-        <div className='col-span-6 ml-auto '>
-          <Grid className='border-2 w-[250px] bg-white ms-auto border-[#D1D1D1] flex self-center py-3 px-4 rounded-xl'>
-            <div className='col-span-4 flex self-center justify-around border-r-2 border-[#D1D1D1]'>
+        <div className="col-span-4"></div>
+        <div className="col-span-2"></div>
+        <div className="col-span-6 ml-auto ">
+          <Grid className="border-2 w-[250px] bg-white ms-auto border-[#D1D1D1] flex self-center py-3 px-4 rounded-xl">
+            <div className="col-span-4 flex self-center justify-around border-r-2 border-[#D1D1D1]">
               {/* <div>
                 <img src={SearchImage} className='cursor-pointer' alt="SearchImage" />
               </div> */}
               <div>
-               <Link to={'/notifications'} ><img src={NotificationImage} className='cursor-pointer' alt="NotificationImage" /> </Link>
+                <Link to={"/notifications"}>
+                  <img
+                    src={NotificationImage}
+                    className="cursor-pointer"
+                    alt="NotificationImage"
+                  />{" "}
+                </Link>
               </div>
               {/* <div>
                 <img src={SettingImage} className='cursor-pointer' alt="SettingImage" />
               </div> */}
             </div>
-            <div className='col-span-8 self-center flex justify-around'>
-                {/* <img src={ProfileImage} alt='ProfileImage'/> */}
-                <p className='text-light-black font-semibold text-base self-center'>Veronica</p>
-                <img src={Down} className='cursor-pointer' alt='ProfileImage'/>
+            <div className="col-span-8 self-center flex justify-around">
+              {/* <img src={ProfileImage} alt='ProfileImage'/> */}
+              <p className="text-light-black font-semibold text-base self-center">
+                Veronica
+              </p>
+              <img src={Down} className="cursor-pointer" alt="ProfileImage" />
             </div>
           </Grid>
         </div>
       </Grid>
     </div>
-  )
+  );
 }
 
-export default Headbar
+export default Headbar;
--- src/pages/notification/notification.js
+++ src/pages/notification/notification.js
@@ -1,103 +1,91 @@
-import React from 'react'
-import Headbar from '../../common/headBar'
-import BackImage from '../../assets/images/icons/backArrow.svg'
-import date from '../../assets/images/icons/date.svg'
-import time from '../../assets/images/icons/time.svg'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import Headbar from "../../common/headBar";
+import BackImage from "../../assets/images/icons/backArrow.svg";
+import date from "../../assets/images/icons/date.svg";
+import time from "../../assets/images/icons/time.svg";
+import { Link } from "react-router-dom";
+
+import { getNotifications } from "../../services/extraServices";
+
 function Notification() {
+  const [notificationList, setNotificationList] = useState([]);
+  useEffect(() => {
+    getNotifications().then((response) => {
+      setNotificationList(response.result.notification);
+      console.log(response.result.notification);
+    });
+  }, []);
   return (
-    <div className='my-8 ml-3 relative overflow-x-hidden'>
-        <Headbar className="!top-0"/>
-        <div className='flex'>
-        <Link to={'/dashboard'} className='h-[60px] w-[60px] flex border-[1px] bg-white border-[#D1D1D1] rounded-[25px]'>
-            <img src={BackImage} className='m-auto my-auto self-center bg-white' alt='BackImage'/>
-          </Link>
-          <div className='pl-3'>
-            <p className='font-semibold text-[36px] leading-9 mb-[3px]'>Notifications</p>
-            <ul className='flex self-center'>
-              <li className='text-sm text-neutral-grey font-Regular'><Link to={'/'}>Servicer </Link>  /  </li>
-              <li className='text-sm text-neutral-grey font-semibold ml-2 pt-[1px]'> Notifications </li>
-            </ul>
-          </div>
+    <div className="my-8 ml-3 relative overflow-x-hidden">
+      <Headbar className="!top-0" />
+      <div className="flex">
+        <Link
+          to={"/dashboard"}
+          className="h-[60px] w-[60px] flex border-[1px] bg-white border-[#D1D1D1] rounded-[25px]"
+        >
+          <img
+            src={BackImage}
+            className="m-auto my-auto self-center bg-white"
+            alt="BackImage"
+          />
+        </Link>
+        <div className="pl-3">
+          <p className="font-semibold text-[36px] leading-9 mb-[3px]">
+            Notifications
+          </p>
+          <ul className="flex self-center">
+            <li className="text-sm text-neutral-grey font-Regular">
+              <Link to={"/"}>Servicer </Link> /{" "}
+            </li>
+            <li className="text-sm text-neutral-grey font-semibold ml-2 pt-[1px]">
+              {" "}
+              Notifications{" "}
+            </li>
+          </ul>
         </div>
+      </div>
 
-            <div className='mt-8'>
-                <div className='border border-[#D9D9D9] rounded-[25px] mb-5 px-6 py-8 mr-4'>
-                    <p className='font-semibold text-lg'>New Registration: Finibus Bonorum et Malorum</p>
-                    <p className='mb-6 text-base text-[#999999] font-Regular'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface <br/> without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                    <div className='flex'>
-                        <div className='flex mr-10 font-Regular'>
-                            <img src={date} className='mr-2' alt='date'/>
-                            <p><b> Date : </b> 24 Nov 2023</p>
-                        </div>
-                        <div className='flex font-Regular'>
-                            <img src={time} className='mr-2' alt='Time'/>
-                            <p><b> Time : </b> 9:30 AM</p>
-                        </div>
-                    </div>
+      <div className="mt-8">
+        {notificationList.length !== 0 ? (
+          notificationList.map((data, key) => (
+            <div
+              key={key}
+              className="border border-[#D9D9D9] rounded-[25px] mb-5 px-6 py-8 mr-4"
+            >
+              <p className="font-semibold text-lg">
+                {data?.notificationData.title}
+              </p>
+              <p className="mb-6 text-base text-[#999999] font-Regular">
+                {data?.notificationData.description}
+              </p>
+              <div className="flex">
+                <div className="flex mr-10 font-Regular">
+                  <img src={date} className="mr-2" alt="date" />
+                  <p>
+                    <b> Date : </b>{" "}
+                    {new Date(
+                      data.notificationData.createdAt
+                    ).toLocaleDateString()}
+                  </p>
                 </div>
-{/* Readed */}
-                <div className='border border-[#D9D9D9] rounded-[25px] mb-5 px-6 py-8 mr-4'>
-                    <p className='font-Regular text-lg'>New Registration: Finibus Bonorum et Malorum</p>
-                    <p className='mb-6 text-base text-[#999999] font-Regular'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface <br/> without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                    <div className='flex'>
-                        <div className='flex mr-10 font-Regular'>
-                            <img src={date} className='mr-2' alt='date'/>
-                            <p><b> Date : </b> 24 Nov 2023</p>
-                        </div>
-                        <div className='flex font-Regular'>
-                            <img src={time} className='mr-2' alt='Time'/>
-                            <p><b> Time : </b> 9:30 AM</p>
-                        </div>
-                    </div>
+                <div className="flex font-Regular">
+                  <img src={time} className="mr-2" alt="Time" />
+                  <p>
+                    <b> Time : </b>{" "}
+                    {new Date(
+                      data.notificationData.createdAt
+                    ).toLocaleTimeString()}
+                  </p>
                 </div>
-
-                {/* End read  */}
-                <div className='border border-[#D9D9D9] rounded-[25px] mb-5 px-6 py-8 mr-4'>
-                    <p className='font-semibold text-lg'>New Registration: Finibus Bonorum et Malorum</p>
-                    <p className='mb-6 text-base text-[#999999] font-Regular'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface <br/> without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                    <div className='flex'>
-                        <div className='flex mr-10 font-Regular'>
-                            <img src={date} className='mr-2' alt='date'/>
-                            <p><b> Date : </b> 24 Nov 2023</p>
-                        </div>
-                        <div className='flex font-Regular'>
-                            <img src={time} className='mr-2' alt='Time'/>
-                            <p><b> Time : </b> 9:30 AM</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='border border-[#D9D9D9] rounded-[25px] mb-5 px-6 py-8 mr-4'>
-                    <p className='font-semibold text-lg'>New Registration: Finibus Bonorum et Malorum</p>
-                    <p className='mb-6 text-base text-[#999999] font-Regular'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface <br/> without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                    <div className='flex'>
-                        <div className='flex mr-10 font-Regular'>
-                            <img src={date} className='mr-2' alt='date'/>
-                            <p><b> Date : </b> 24 Nov 2023</p>
-                        </div>
-                        <div className='flex font-Regular'>
-                            <img src={time} className='mr-2' alt='Time'/>
-                            <p><b> Time : </b> 9:30 AM</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='border border-[#D9D9D9] rounded-[25px] mb-5 px-6 py-8 mr-4'>
-                    <p className='font-semibold text-lg'>New Registration: Finibus Bonorum et Malorum</p>
-                    <p className='mb-6 text-base text-[#999999] font-Regular'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface <br/> without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                    <div className='flex'>
-                        <div className='flex mr-10 font-Regular'>
-                            <img src={date} className='mr-2' alt='date'/>
-                            <p><b> Date : </b> 24 Nov 2023</p>
-                        </div>
-                        <div className='flex font-Regular'>
-                            <img src={time} className='mr-2' alt='Time'/>
-                            <p><b> Time : </b> 9:30 AM</p>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
-        </div>
-  )
+          ))
+        ) : (
+          <p>No Notification</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Notification
+export default Notification;
