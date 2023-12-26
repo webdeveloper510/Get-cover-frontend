7bb3c5c6b54e1684a6f7c42e235ad59b4de811e4
Saurav Sharma <saurav@codenomad.net>
2023-12-26 12:48:45 +0530

    changes

--- src/pages/auth/newPassword.js
+++ src/pages/auth/newPassword.js
@@ -122,7 +122,7 @@ function NewPassword() {
 
       {/* Modal Email Popop */}
       <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <Button
+        {/* <Button
           onClick={closeModal}
           className="absolute right-[-13px] top-0 h-[80px] w-[80px] !p-[19px] mt-[-9px] !rounded-full !bg-[#5f5f5f]"
         >
@@ -130,7 +130,7 @@ function NewPassword() {
             src={Cross}
             className="w-full h-full text-black rounded-full p-0"
           />
-        </Button>
+        </Button> */}
         <div className="text-center py-3">
           <img src={NewPasswordEmail} alt="email Image" className="mx-auto" />
           <p className="text-3xl mb-0 mt-4 font-semibold text-neutral-grey">
