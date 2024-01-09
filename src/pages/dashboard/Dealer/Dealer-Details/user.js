5598866450857394d3a1c94d6fa88113bc797328
Saurav <saurav@codenomad.net>
2024-01-09 06:49:38 -0800

    chnages

--- src/pages/dashboard/Dealer/Dealer-Details/user.js
+++ src/pages/dashboard/Dealer/Dealer-Details/user.js
@@ -436,7 +436,7 @@ function UserList(props) {
             </div>
             <div className="col-span-7">
               <div className="bg-[#F9F9F9] rounded-[30px] p-3 border-[1px] border-[#D1D1D1]">
-                <form className="mt-8" onSubmit={formikUSerFilter.handleSubmit}>
+                <form className="" onSubmit={formikUSerFilter.handleSubmit}>
                   <Grid className="!grid-cols-11">
                     <div className="col-span-3 self-center">
                       <Input
@@ -454,7 +454,7 @@ function UserList(props) {
                     <div className="col-span-3 self-center">
                       <Input
                         name="email"
-                        type="email"
+                        type="text"
                         className="!text-[14px] !bg-[#f7f7f7]"
                         className1="!text-[13px] !pt-1 placeholder-opacity-50 !pb-1 placeholder-[#1B1D21] !bg-[white]"
                         label=""
