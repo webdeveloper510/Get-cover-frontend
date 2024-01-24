c3b904067c59d60b5f0e77908420fb5812627573
Saurav Sharma <saurav@codenomad.net>
2024-01-24 19:05:28 +0530

    fixed change told by Nikhil sir

--- src/common/model.js
+++ src/common/model.js
@@ -7,7 +7,7 @@ const Modal = ({ isOpen, onClose, children }) => {
     <div className="fixed top-0 left-0 h-full w-full flex items-center justify-center z-[99999] bg-[#333333c7] backdrop-blur-xl	 ">
       <div className="modal-overlay" onClick={onClose}></div>
       <div className="modal-container w-[700px] max-w-[95%] relative">
-        <div className="bg-white rounded-[50px] shadow-lg p-4">{children}</div>
+        <div className="bg-white rounded-[50px] shadow-lg p-4 h-full">{children}</div>
       </div>
     </div>
   );
