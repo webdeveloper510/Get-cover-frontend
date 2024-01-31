b3d17942a64a4e54084ed5100e58b80b2e9ae003
neek3190 <neek3190@gmail.com>
2024-01-31 16:11:33 +0530

    changes

--- src/pages/dashboard/Order/orderList.js
+++ src/pages/dashboard/Order/orderList.js
@@ -1,26 +1,26 @@
-import React , { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import Button from '../../../common/button'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Button from "../../../common/button";
 
-import ActiveIcon from '../../../assets/images/icons/iconAction.svg';
+import ActiveIcon from "../../../assets/images/icons/iconAction.svg";
 import clearFilter from "../../../assets/images/icons/Clear-Filter-Icon-White.svg";
-import AddItem from '../../../assets/images/icons/addItem.svg';
-import Search from '../../../assets/images/icons/SearchIcon.svg';
+import AddItem from "../../../assets/images/icons/addItem.svg";
+import Search from "../../../assets/images/icons/SearchIcon.svg";
 import AddDealer from "../../../assets/images/dealer-book.svg";
-import Headbar from '../../../common/headBar';
+import Headbar from "../../../common/headBar";
 import shorting from "../../../assets/images/icons/shorting.svg";
-import Grid from '../../../common/grid';
-import Input from '../../../common/input';
-import DataTable from "react-data-table-component"
-import Select from '../../../common/select';
-import { RotateLoader } from 'react-spinners';
-import { getOrders } from '../../../services/orderServices';
-import Modal from '../../../common/model';
+import Grid from "../../../common/grid";
+import Input from "../../../common/input";
+import DataTable from "react-data-table-component";
+import Select from "../../../common/select";
+import { RotateLoader } from "react-spinners";
+import { getOrders } from "../../../services/orderServices";
+import Modal from "../../../common/model";
 
 function OrderList() {
   const [selectedAction, setSelectedAction] = useState(null);
-  const [selectedProduct, setSelectedProduct] = useState('');
-  const [orderList,setOrderList]=useState([]);
+  const [selectedProduct, setSelectedProduct] = useState("");
+  const [orderList, setOrderList] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
@@ -30,42 +30,39 @@ function OrderList() {
   const closeModal = () => {
     setIsModalOpen(false);
   };
-  const handleSelectChange1 = (label , value) => {
-    console.log(label, value , "selected")
+  const handleSelectChange1 = (label, value) => {
+    console.log(label, value, "selected");
     setSelectedProduct(value);
   };
   const [loading, setLoading] = useState(false);
 
   const status = [
-    { label: 'Active', value: true },
-    { label: 'Waiting', value: false },
-    { label: 'Expired', value: false },
-    { label: 'Canceled', value: false },
-    { label: 'Refunded', value: false },
+    { label: "Active", value: true },
+    { label: "Waiting", value: false },
+    { label: "Expired", value: false },
+    { label: "Canceled", value: false },
+    { label: "Refunded", value: false },
   ];
 
-  useEffect(()=>{
-   
-    getOrderList()  
-   
-  },[])
+  useEffect(() => {
+    getOrderList();
+  }, []);
 
-  const getOrderList = async() =>{
+  const getOrderList = async () => {
     setLoading(true);
-    const result =await getOrders({});
-    console.log(result.result)
-    setOrderList(result.result)
+    const result = await getOrders({});
+    console.log(result.result);
+    setOrderList(result.result);
     setLoading(false);
-  }
+  };
   const calculateDropdownPosition = (index) => {
     const isCloseToBottom = orderList.length - index <= 2;
     return isCloseToBottom ? "bottom-[1rem]" : "top-[1rem]";
   };
 
-  
   const paginationOptions = {
-    rowsPerPageText: 'Rows per page:',
-    rangeSeparatorText: 'of',
+    rowsPerPageText: "Rows per page:",
+    rangeSeparatorText: "of",
   };
 
   const CustomNoDataComponent = () => (
@@ -73,21 +70,20 @@ function OrderList() {
       <p>No records found.</p>
     </div>
   );
- 
 
   const columns = [
     {
       name: "ID",
       selector: (row) => row?.unique_key,
       sortable: true,
-      minWidth:'auto',
-      maxWidth:'70px'
+      minWidth: "auto",
+      maxWidth: "70px",
     },
     {
       name: "Dealer Order #",
       selector: (row) => row.venderOrder,
       sortable: true,
-      minWidth:'180px',
+      minWidth: "180px",
     },
     {
       name: "Dealer",
@@ -106,149 +102,211 @@ function OrderList() {
     },
     {
       name: "# of Products",
-      selector: (row) => row.noOfProducts,
+      selector: (row) => row.productsArray[0].noOfProducts,
       sortable: true,
-      minWidth:'150px',
+      minWidth: "150px",
     },
     {
       name: "Order Value",
       selector: (row) => `$ ${row.orderAmount?.toFixed(2)}`,
       sortable: true,
-      minWidth:'150px',
+      minWidth: "150px",
     },
     {
-        name: "Status",
-        cell: (row) => (
-            <div className="relative">
-              <div
-                className={` ${
-                  row.status === true ? "bg-[#6BD133]" : "bg-[#FF4747]"
-                } absolute h-3 w-3 rounded-full top-[33%] ml-[8px]`}
-              ></div>
-              <select
-                value={row.status === true ? "active" : "inactive"}
-                // onChange={(e) => handleStatusChange(row, e.target.value)}
-                className="text-[12px] border border-gray-300 text-[#727378] rounded pl-[20px] py-2 pr-1 font-semibold rounded-xl"
-              >
-                <option value="Active">Active</option>
-                <option value="Waiting">Waiting</option>
-                <option value="Expired">Expired</option>
-                <option value="Canceled">Canceled</option>
-                <option value="Refunded">Refunded</option>
-              </select>
-            </div>
-          ),
-        sortable: true,
+      name: "Status",
+      cell: (row) => (
+        <div className="relative">
+          <div
+            className={` ${
+              row.status === true ? "bg-[#6BD133]" : "bg-[#FF4747]"
+            } absolute h-3 w-3 rounded-full top-[33%] ml-[8px]`}
+          ></div>
+          <select
+            value={row.status === true ? "active" : "inactive"}
+            // onChange={(e) => handleStatusChange(row, e.target.value)}
+            className="text-[12px] border border-gray-300 text-[#727378] rounded pl-[20px] py-2 pr-1 font-semibold rounded-xl"
+          >
+            <option value="Active">Active</option>
+            <option value="Waiting">Waiting</option>
+            <option value="Expired">Expired</option>
+            <option value="Canceled">Canceled</option>
+            <option value="Refunded">Refunded</option>
+          </select>
+        </div>
+      ),
+      sortable: true,
     },
     {
       name: "Action",
-      minWidth:'auto',
-      maxWidth:'80px',
+      minWidth: "auto",
+      maxWidth: "80px",
       cell: (row, index) => {
         // console.log(index, index % 10 == 9)
         return (
           <div className="relative">
-          <div onClick={() => setSelectedAction(selectedAction === row.unique_key ? null : row.unique_key)}>
-            <img src={ActiveIcon} className='cursor-pointer	w-[35px]' alt="Active Icon" />
-          </div>
-          {selectedAction === row.unique_key && (
-            <div className={`absolute z-[2] w-[120px] drop-shadow-5xl -right-3 mt-2 p-2 bg-white border rounded-lg shadow-md ${calculateDropdownPosition(
-              index
-            )}`}>
-              {/* <img src={arrowImage} className={`absolute  object-contain left-1/2 w-[12px] ${index%10 === 9 ? 'bottom-[-5px] rotate-180' : 'top-[-5px]'} `} alt='up arror'/> */}
-                <div className='text-center py-1 border-b'>Edit</div>
-                <div className='text-center py-1 border-b cursor-pointer' onClick={()=> openModal()}>Process Order</div>
-                <div className='text-center py-1'><Link to={'/orderDetails'}>View</Link></div>
+            <div
+              onClick={() =>
+                setSelectedAction(
+                  selectedAction === row.unique_key ? null : row.unique_key
+                )
+              }
+            >
+              <img
+                src={ActiveIcon}
+                className="cursor-pointer	w-[35px]"
+                alt="Active Icon"
+              />
             </div>
-          )}
-        </div>
-        )
-      }
+            {selectedAction === row.unique_key && (
+              <div
+                className={`absolute z-[2] w-[120px] drop-shadow-5xl -right-3 mt-2 p-2 bg-white border rounded-lg shadow-md ${calculateDropdownPosition(
+                  index
+                )}`}
+              >
+                {/* <img src={arrowImage} className={`absolute  object-contain left-1/2 w-[12px] ${index%10 === 9 ? 'bottom-[-5px] rotate-180' : 'top-[-5px]'} `} alt='up arror'/> */}
+                <div className="text-center py-1 border-b">Edit</div>
+                <div
+                  className="text-center py-1 border-b cursor-pointer"
+                  onClick={() => openModal()}
+                >
+                  Process Order
+                </div>
+                <div className="text-center py-1">
+                  <Link to={"/orderDetails"}>View</Link>
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      },
     },
   ];
 
   return (
     <>
-      <div className='my-8 ml-3'>
-
+      <div className="my-8 ml-3">
         <Headbar />
 
-        <div className='flex mt-2'>
-          <div className='pl-3'>
-            <p className='font-bold text-[36px] leading-9	mb-[3px]'>Order</p>
-            <ul className='flex self-center'>
-              <li className='text-sm text-neutral-grey font-Regular'><Link to={'/'}>Order </Link>  /  </li>
-              <li className='text-sm text-neutral-grey font-semibold ml-2 pt-[1px]'> Order List </li>
+        <div className="flex mt-2">
+          <div className="pl-3">
+            <p className="font-bold text-[36px] leading-9	mb-[3px]">Order</p>
+            <ul className="flex self-center">
+              <li className="text-sm text-neutral-grey font-Regular">
+                <Link to={"/"}>Order </Link> /{" "}
+              </li>
+              <li className="text-sm text-neutral-grey font-semibold ml-2 pt-[1px]">
+                {" "}
+                Order List{" "}
+              </li>
             </ul>
           </div>
         </div>
 
-        <Button className="!bg-white flex self-center mb-4 rounded-xl ml-auto border-[1px] border-[#D1D1D1]" > <Link to={'/addOrder'} className='flex'> <img src={AddItem} className='self-center' alt='AddItem' /> <span className='text-black ml-3 text-[14px] font-Regular'> Add New Order </span>  </Link></Button>
-  
-        <div className='bg-white mt-6 border-[1px] border-[#D1D1D1] rounded-xl'>
-          <Grid className='!p-[26px] !pt-[14px] !pb-0'>
-            <div className='col-span-5 self-center'>
-              <p className='text-xl font-semibold'>Order List</p>
+        <Button className="!bg-white flex self-center mb-4 rounded-xl ml-auto border-[1px] border-[#D1D1D1]">
+          {" "}
+          <Link to={"/addOrder"} className="flex">
+            {" "}
+            <img src={AddItem} className="self-center" alt="AddItem" />{" "}
+            <span className="text-black ml-3 text-[14px] font-Regular">
+              {" "}
+              Add New Order{" "}
+            </span>{" "}
+          </Link>
+        </Button>
+
+        <div className="bg-white mt-6 border-[1px] border-[#D1D1D1] rounded-xl">
+          <Grid className="!p-[26px] !pt-[14px] !pb-0">
+            <div className="col-span-5 self-center">
+              <p className="text-xl font-semibold">Order List</p>
             </div>
-            <div className='col-span-7'>
-              <div className='bg-[#F9F9F9] rounded-[30px] p-3 border-[1px] border-[#D1D1D1]'>
-                <Grid className='!grid-cols-11' >
-                  <div className='col-span-3 self-center'>
-                    <Input name='Name' type='text' className='!text-[14px] !bg-[#f7f7f7]' className1="!text-[13px] !pt-1 placeholder-opacity-50 !pb-1 placeholder-[#1B1D21] !bg-[white]" label='' placeholder='ID' />
+            <div className="col-span-7">
+              <div className="bg-[#F9F9F9] rounded-[30px] p-3 border-[1px] border-[#D1D1D1]">
+                <Grid className="!grid-cols-11">
+                  <div className="col-span-3 self-center">
+                    <Input
+                      name="Name"
+                      type="text"
+                      className="!text-[14px] !bg-[#f7f7f7]"
+                      className1="!text-[13px] !pt-1 placeholder-opacity-50 !pb-1 placeholder-[#1B1D21] !bg-[white]"
+                      label=""
+                      placeholder="ID"
+                    />
                   </div>
-                  <div className='col-span-3 self-center'>
-                    <Input name='orderNo' type='text'className='!text-[14px] !bg-[#f7f7f7]' className1="!text-[13px] !pt-1 placeholder-opacity-50 !pb-1 placeholder-[#1B1D21] !bg-[white]" label='' placeholder='Dealer Order No.' />
+                  <div className="col-span-3 self-center">
+                    <Input
+                      name="orderNo"
+                      type="text"
+                      className="!text-[14px] !bg-[#f7f7f7]"
+                      className1="!text-[13px] !pt-1 placeholder-opacity-50 !pb-1 placeholder-[#1B1D21] !bg-[white]"
+                      label=""
+                      placeholder="Dealer Order No."
+                    />
                   </div>
-                  <div className='col-span-3 self-center'>
-                  <Select label=""
+                  <div className="col-span-3 self-center">
+                    <Select
+                      label=""
                       options={status}
-                      color='text-[#1B1D21] opacity-50'
+                      color="text-[#1B1D21] opacity-50"
                       className1="!pt-1 !pb-1 !text-[13px] !bg-[white]"
                       className="!text-[14px] !bg-[#f7f7f7]"
                       selectedValue={selectedProduct}
-                      onChange={handleSelectChange1} />
+                      onChange={handleSelectChange1}
+                    />
                   </div>
-                  
-                  <div className='col-span-2 self-center flex'>
-                    <img src={Search} className='cursor-pointer	'  alt='Search' />
-                    <Button type="submit" className=' !bg-transparent !p-0'>
-                        <img
-                          src={clearFilter}
-                          className="cursor-pointer	mx-auto"
-                          alt="clearFilter"
-                        />
-                      </Button>
+
+                  <div className="col-span-2 self-center flex">
+                    <img src={Search} className="cursor-pointer	" alt="Search" />
+                    <Button type="submit" className=" !bg-transparent !p-0">
+                      <img
+                        src={clearFilter}
+                        className="cursor-pointer	mx-auto"
+                        alt="clearFilter"
+                      />
+                    </Button>
                   </div>
                 </Grid>
-
               </div>
             </div>
           </Grid>
-          <div className='mb-5 relative'>
-          {loading ? (
+          <div className="mb-5 relative">
+            {loading ? (
               <div className=" h-[400px] w-full flex py-5">
                 <div className="self-center mx-auto">
                   <RotateLoader color="#333" />
                 </div>
               </div>
             ) : (
-            <DataTable columns={columns} data={orderList} highlightOnHover sortIcon={<> <img src={shorting}  className="ml-2" alt="shorting"/> </>} pagination  paginationPerPage={10} noDataComponent={<CustomNoDataComponent />} paginationComponentOptions={paginationOptions} paginationRowsPerPageOptions={[10, 20, 50, 100]} />
+              <DataTable
+                columns={columns}
+                data={orderList}
+                highlightOnHover
+                sortIcon={
+                  <>
+                    {" "}
+                    <img src={shorting} className="ml-2" alt="shorting" />{" "}
+                  </>
+                }
+                pagination
+                paginationPerPage={10}
+                noDataComponent={<CustomNoDataComponent />}
+                paginationComponentOptions={paginationOptions}
+                paginationRowsPerPageOptions={[10, 20, 50, 100]}
+              />
             )}
           </div>
         </div>
-
       </div>
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         <div className="text-center py-3">
           <img src={AddDealer} alt="email Image" className="mx-auto" />
 
           <p className="text-3xl mb-0 mt-4 font-bold text-neutral-grey ">
-             
-            <span className="text-light-black"> Order Processed </span> Successfully
+            <span className="text-light-black"> Order Processed </span>{" "}
+            Successfully
           </p>
 
           <p className="text-neutral-grey text-base font-medium mt-2">
-           Order Processed Successfully
+            Order Processed Successfully
           </p>
           <p className="text-neutral-grey text-base font-medium mt-2">
             Redirecting you on Order List Page 3 seconds.
@@ -256,7 +314,7 @@ function OrderList() {
         </div>
       </Modal>
     </>
-  )
+  );
 }
 
-export default OrderList
+export default OrderList;
