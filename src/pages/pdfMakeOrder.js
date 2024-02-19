1aadcdce47c92aeb5cfb10a0fa881ba5fc4bcf4d
Saurav Sharma <saurav@codenomad.net>
2024-02-19 13:29:41 +0530

    changes

--- src/pages/pdfMakeOrder.js
+++ src/pages/pdfMakeOrder.js
@@ -23,7 +23,7 @@ const PdfMake = (props) => {
   const downloadAsPDF = () => {
     const opt = {
       margin: 0.5,
-      filename: "invoice.pdf",
+      filename: "Export-Order.pdf",
       image: { type: "jpeg", quality: 0.98 },
       html2canvas: { scale: 2 },
       jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
