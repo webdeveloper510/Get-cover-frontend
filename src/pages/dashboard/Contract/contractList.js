e837c0acf18327e17f6e3f0b062922a3569d95ec
Saurav Sharma <saurav@codenomad.net>
2024-02-21 16:04:00 +0530

    changes

--- src/pages/dashboard/Contract/contractList.js
+++ src/pages/dashboard/Contract/contractList.js
@@ -191,18 +191,18 @@ function ContractList() {
                   return (
                     <div className="px-3 mt-5">
                       <div>
-                        <Grid className="bg-[#333333] !gap-2 !grid-cols-9 rounded-t-xl">
+                        <Grid className="bg-[#333333] !gap-2 !grid-cols-11 rounded-t-xl">
                           <div className="col-span-3 self-center text-center bg-contract bg-cover bg-right bg-no-repeat rounded-ss-xl">
                             <p className="text-white py-2 font-Regular">
                               Contract ID : <b> {res.unique_key} </b>
                             </p>
                           </div>
-                          <div className="col-span-2 self-center text-center bg-contract bg-cover bg-right bg-no-repeat ">
+                          <div className="col-span-3 self-center text-center bg-contract bg-cover bg-right bg-no-repeat ">
                             <p className="text-white py-2 font-Regular">
                               Order ID : <b> {res?.order[0]?.unique_key} </b>
                             </p>
                           </div>
-                          <div className="col-span-2 self-center text-center bg-contract bg-cover bg-right bg-no-repeat ">
+                          <div className="col-span-3 self-center text-center bg-contract bg-cover bg-right bg-no-repeat ">
                             <p className="text-white py-2 font-Regular">
                               Dealer P.O. No. :{" "}
                               <b> {res?.order[0]?.venderOrder} </b>
