475fcc4cb7a7fb55cb338c7a11d85f38401c00b4
Saurav Sharma <saurav@codenomad.net>
2024-04-09 20:24:13 +0530

    ISSUE FIXED

--- src/pages/Reseller-screens/user.js
+++ src/pages/Reseller-screens/user.js
@@ -24,29 +24,41 @@ import {
   getUserListByDealerId,
   userDetailsById,
   updateUserDetailsById,
+  addUserToCustomerPortal,
+  changePrimaryByUserIdCustomerPortal,
 } from "../../services/userServices";
 import Select from "../../common/select";
-import { getCustomerUsersById } from "../../services/customerServices";
+import { getCustomerDetailsByIdCustomerPortal, getCustomerUsersById, getCustomerUsersByIdCustomerPortal } from "../../services/customerServices";
 import { useMyContext } from "../../context/context";
 import AddItem from "../../assets/images/icons/addItem.svg";
 import Headbar from "../../common/headBar";
 import RadioButton from "../../common/radio";
 import Tabs from "../../common/tabs";
+import { addSuperAdminMembers, changePasswordbyToken } from "../../services/extraServices";
+import make from "../../assets/images/star.png";
+import edit from "../../assets/images/edit-text.png";
+import delete1 from "../../assets/images/delete.png";
 
 function ResellerUser() {
   const { toggleFlag } = useMyContext();
   const [selectedAction, setSelectedAction] = useState(null);
   const [userList, setUserList] = useState([]);
+  const [isUserModalOpen, setIsUserModalOpen] = useState(false);
   const [isModalOpen, SetIsModalOpen] = useState(false);
   const [isprimary, SetIsprimary] = useState(false);
   const [mainStatus, setMainStatus] = useState(true);
+  const [details, setDetails] = useState(true);
   const [servicerStatus, setServiceStatus] = useState(true);
+  const [firstMessage, setFirstMessage] = useState("");
+  const [secondMessage, setSecondMessage] = useState("");
   const [deleteId, setDeleteId] = useState("");
-
+  const [isPasswordOpen, setIsPasswordOpen] = useState(false);
+  const [createAccountOption, setCreateAccountOption] = useState("yes");
   const [primaryText, SetPrimaryText] = useState("");
   const [secondaryText, SetSecondaryText] = useState("");
   const [timer, setTimer] = useState(3);
   const dropdownRef = useRef(null);
+  const [loading1, setLoading1] = useState(false);
 
   const [isModalOpen12, setIsModalOpen12] = useState(false);
   const [initialFormValues, setInitialFormValues] = useState({
@@ -57,13 +69,38 @@ function ResellerUser() {
     status: true,
     id: "",
   });
+  const closeUserModal = () => {
+    setIsUserModalOpen(false);
+    setInitialFormValues({
+      lastName: "",
+      firstName: "",
+      phoneNumber: "",
+      position: "",
+      status: true,
+      id: "",
+    });
+  };
   // console.log("toggleFlag", toggleFlag);
   const [loading, setLoading] = useState(false);
-
+  const handleRadioChange = (event) => {
+    const selectedValue = event.target.value;
+    console.log(selectedValue);
+    formik.setFieldValue("status", selectedValue === "yes" ? true : false);
+    setCreateAccountOption(selectedValue);
+  };
   const getUserList = async () => {
-    const result = await getCustomerUsersById("", {});
+    const result = await getCustomerUsersByIdCustomerPortal();
     console.log(result.result);
     setUserList(result.result);
+    
+  };
+  const getCustomerDetails = async () => {
+    setLoading1(true);
+    const result = await getCustomerDetailsByIdCustomerPortal();
+    console.log(result.result);
+    setDetails(result.result);
+    SetIsprimary(result.loginMember.isPrimary);
+    setLoading1(false);
   };
   const handleClickOutside = (event) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -73,6 +110,7 @@ function ResellerUser() {
   };
   useEffect(() => {
     getUserList();
+    getCustomerDetails();
   }, []);
   useEffect(() => {
     document.addEventListener("click", handleClickOutside);
@@ -140,14 +178,25 @@ function ResellerUser() {
   const [isModalOpen2, setIsModalOpen2] = useState(false);
   const closeModal2 = () => {
     setIsModalOpen2(false);
-    formik.resetForm();
+    userValues.resetForm();
   };
   const openModal2 = () => {
     setIsModalOpen2(true);
   };
-
+  const openUserModal = () => {
+    setInitialFormValues({
+      lastName: "",
+      firstName: "",
+      phoneNumber: "",
+      position: "",
+      status: true,
+      id: "",
+    });
+    setIsUserModalOpen(true);
+  };
   const closeModal12 = () => {
     setIsModalOpen12(false);
+    formik.resetForm();
   };
   const openModal12 = () => {
     setIsModalOpen12(true);
@@ -177,6 +226,7 @@ function ResellerUser() {
       console.error("Error in handleStatusChange:", error);
     }
   };
+  const emailValidationRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
   const formik = useFormik({
     initialValues: initialFormValues,
     enableReinitialize: true,
@@ -187,6 +237,10 @@ function ResellerUser() {
       lastName: Yup.string()
         .required("Required")
         .max(30, "Must be exactly 30 characters"),
+      email: Yup.string()
+        .required("Required")
+        .matches(emailValidationRegex, "Invalid email address")
+        .transform((originalValue) => originalValue.trim()),
       phoneNumber: Yup.string()
         .required("Required")
         .min(10, "Must be at least 10 characters")
@@ -197,24 +251,18 @@ function ResellerUser() {
     onSubmit: async (values) => {
       console.log("Form values:", values);
       setLoading(true);
-      const result = await updateUserDetailsById(values);
+      const result = await addUserToCustomerPortal(values);
       console.log(result);
       if (result.code == 200) {
         setLoading(false);
-        SetPrimaryText("User Edited Successfully ");
-        SetSecondaryText("user edited successfully ");
+        SetPrimaryText("User Added Successfully ");
+        SetSecondaryText("user Added successfully ");
         openModal();
         toggleFlag();
-        // setIsModalOpen3(true);
-
-        // setError(result.message);
         setTimer(3);
         getUserList();
       } else {
         setLoading(false);
-        // setError(false);
-        // setIsModalOpen(true);
-        // setTimer(3);
       }
       closeModal2();
     },
@@ -243,7 +291,7 @@ function ResellerUser() {
     console.log(id);
     const result = await userDetailsById(id);
     console.log(result.result.status);
-    SetIsprimary(result.result.isPrimary);
+    
     setMainStatus(result.mainStatus);
     setInitialFormValues({
       id: id,
@@ -253,13 +301,12 @@ function ResellerUser() {
       position: result?.result?.position,
       status: result?.result?.status,
     });
-
     openModal2();
   };
 
   const makeUserPrimary = async (row) => {
     console.log(row._id);
-    const result = await changePrimaryByUserId(row._id);
+    const result = await changePrimaryByUserIdCustomerPortal(row._id);
     console.log(result);
     if (result.code === 200) {
       SetPrimaryText("It's set to Primary");
@@ -272,7 +319,7 @@ function ResellerUser() {
   const filterUserDetails = async (data) => {
     try {
       setLoading(true);
-      const res = await getUserListByDealerId("", data);
+      const res = await getCustomerUsersByIdCustomerPortal("", data);
       setUserList(res.result);
     } catch (error) {
       console.error("Error fetching category list:", error);
@@ -301,6 +348,18 @@ function ResellerUser() {
       filterUserDetails(values);
     },
   });
+
+  const formatPhoneNumber = (phoneNumber) => {
+    const cleaned = ('' + phoneNumber).replace(/\D/g, ''); // Remove non-numeric characters
+    const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/); // Match groups of 3 digits
+  
+    if (match) {
+      return `(${match[1]}) ${match[2]}-${match[3]}`;
+    }
+  
+    return phoneNumber; // Return original phone number if it couldn't be formatted
+  };
+  
   const columns = [
     {
       name: "Name",
@@ -324,7 +383,7 @@ function ResellerUser() {
     },
     {
       name: "Phone Number",
-      selector: (row) => row.phoneNumber,
+      selector: (row) => formatPhoneNumber(row.phoneNumber),
       sortable: true,
     },
     {
@@ -377,38 +436,41 @@ function ResellerUser() {
               />
             </div>
             {selectedAction === row.email && (
-              <div
-                ref={dropdownRef}
-                className={`absolute z-[9999] ${
-                  !row.isPrimary ? "w-[120px]" : "w-[80px]"
-                } drop-shadow-5xl -right-3 mt-2 bg-white border rounded-lg shadow-md ${calculateDropdownPosition(
-                  index
-                )}`}
-              >
-                {!row.isPrimary && row.status && (
-                  <div
-                    className="text-center py-2 cursor-pointer border-b"
-                    onClick={() => makeUserPrimary(row)}
-                  >
-                    Make Primary
-                  </div>
-                )}
+               <div
+               ref={dropdownRef}
+               className={`absolute z-[9999] ${
+                 !row.isPrimary ? "w-[140px]" : "w-[80px]"
+               } drop-shadow-5xl -right-3 mt-2 py-1 bg-white border rounded-lg shadow-md ${calculateDropdownPosition(
+                 index
+               )}`}
+             >
+               {!row.isPrimary && row.status && (
+                 <div
+                   className="text-left cursor-pointer flex hover:font-semibold py-1 px-2 border-b"
+                   onClick={() => makeUserPrimary(row)}
+                 >
+                  <img src={make} className="w-4 h-4 mr-2" />{" "}
+                   <span className="self-center"> Make Primary </span>
+                 </div>
+               )}
 
-                <div
-                  className="text-center py-2 cursor-pointer border-b"
-                  onClick={() => editUser(row._id)}
-                >
-                  Edit
-                </div>
-                {!row.isPrimary && (
-                  <div
-                    className="text-center text-red-500 py-2 cursor-pointer"
-                    onClick={() => openModal1(row._id)}
-                  >
-                    Delete
-                  </div>
-                )}
-              </div>
+               <div
+                 className="text-left cursor-pointer flex hover:font-semibold py-1 px-2 border-b"
+                 onClick={() => editUser(row._id)}
+               >
+                  <img src={edit} className="w-4 h-4 mr-2" />{" "}
+                 <span className="self-center">Edit </span>
+               </div>
+               {!row.isPrimary && (
+                 <div
+                   className="text-left cursor-pointer flex hover:font-semibold py-1 px-2"
+                   onClick={() => openModal1(row._id)}
+                 >
+                  <img src={delete1} className="w-4 h-4 mr-2" />{" "}
+                   <span className="self-center">Delete</span>
+                 </div>
+               )}
+             </div>
             )}
           </div>
         );
@@ -416,11 +478,163 @@ function ResellerUser() {
     },
   ];
 
+  const columns12 = [
+    {
+      name: "Name",
+      selector: "name",
+      // sortable: true,
+      cell: (row) => (
+        <div className="flex relative">
+          {row.isPrimary && (
+            <img src={star} alt="" className="absolute -left-3 top-0" />
+          )}
+          <span className="self-center pt-2 ml-3">
+            {row.firstName} {row.lastName}
+          </span>
+        </div>
+      ),
+    },
+    {
+      name: "Email Address",
+      selector: (row) => row.email,
+      // sortable: true,
+    },
+    {
+      name: "Phone Number",
+      selector: (row) => formatPhoneNumber(row.phoneNumber),
+      // sortable: true,
+    },
+    {
+      name: "Position",
+      selector: (row) => row.position,
+      // sortable: true,
+    },
+    {
+      name: "Status",
+      selector: (row) => row.status,
+      // sortable: true,
+      cell: (row) => (
+        <div className="relative">
+          <div
+            className={` ${
+              row.status === true ? "bg-[#6BD133]" : "bg-[#FF4747]"
+            } absolute h-3 w-3 rounded-full top-[33%] ml-[8px]`}
+          ></div>
+          <select
+            disabled={true}
+            value={row.status === true ? "active" : "inactive"}
+            onChange={(e) => handleStatusChange(row, e.target.value)}
+            className="text-[12px] border border-gray-300 text-[#727378] rounded pl-[20px] py-2 pr-1 font-semibold rounded-xl"
+          >
+            <option value="active">Active</option>
+            <option value="inactive">Inactive</option>
+          </select>
+        </div>
+      ),
+    },
+  ];
+  const initialValues2 = {
+    oldPassword: "",
+    newPassword: "",
+    confirmPassword: "",
+  };
+
+  const handleSubmit = (values, { setSubmitting }) => {
+    console.log(values);
+    passwordChange(values);
+    setSubmitting(false);
+  };
+
+  const passwordChange = async (value) => {
+    setLoading1(true);
+    delete value.confirmPassword;
+
+    try {
+      const res = await changePasswordbyToken(value);
+      console.log(res);
+      if (res.code == 200) {
+        setFirstMessage("Edit  Successfully ");
+        setSecondMessage("User Password edited  successfully ");
+        SetIsModalOpen(true);
+        setTimer(3); }
+        else {
+          setFirstMessage("Error");
+          setSecondMessage(res.message);
+          setIsPasswordOpen(true);
+        }
+    } catch (error) {
+      console.error("Error changing password:", error);
+    } finally {
+      setLoading1(false);
+    }
+    console.log(value);
+  };
+  const passwordChnageForm = useFormik({
+    initialValues: initialValues2,
+    validationSchema: Yup.object({
+      oldPassword: Yup.string().required("Required"),
+      newPassword: Yup.string().required("Required"),
+      confirmPassword: Yup.string()
+        .required("Required")
+        .oneOf([Yup.ref("newPassword"), null], "Passwords must match"),
+    }),
+    onSubmit: handleSubmit,
+  });
+  const closePassword = () => {
+    setIsPasswordOpen(false);
+  };
   const CustomNoDataComponent = () => (
     <div className="text-center my-5">
       <p>No records found.</p>
     </div>
   );
+  const userValues = useFormik({
+    initialValues: initialFormValues,
+    enableReinitialize: true,
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .required("Required")
+        .transform((originalValue) => originalValue.trim())
+        .max(30, "Must be exactly 30 characters"),
+      lastName: Yup.string()
+        .required("Required")
+        .transform((originalValue) => originalValue.trim())
+        .max(30, "Must be exactly 30 characters"),
+      email: Yup.string()
+        .required("Required")
+        .matches(emailValidationRegex, "Invalid email address")
+        .transform((originalValue) => originalValue.trim()),
+      phoneNumber: Yup.string()
+        .required("Required")
+        .min(10, "Must be at least 10 characters")
+        .max(10, "Must be exactly 10 characters")
+        .matches(/^[0-9]+$/, "Must contain only digits"),
+      status: Yup.boolean().required("Required"),
+    }),
+    onSubmit: async (values) => {
+      console.log("Form values:", values);
+      setLoading(true);
+      const result = await addSuperAdminMembers(values);
+      console.log(result);
+      if (result.code == 200) {
+        setLoading(false);
+        setTimer(3);
+        SetPrimaryText("User Add Successfully ");
+        SetSecondaryText("user Add successfully ");
+       
+        SetIsModalOpen(true);
+        setIsUserModalOpen(false);
+        getUserList();
+      } else {
+        setLoading(false);
+        if (result.code === 401) {
+          userValues.setFieldError("email", "Email already in use");
+        }
+        SetIsModalOpen(true);
+      }
+      closeModal2();
+    },
+  });
 
   return (
     <>
