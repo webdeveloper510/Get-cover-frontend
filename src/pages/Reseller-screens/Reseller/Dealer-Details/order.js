a6577f4b6530bbac238134195c887f39a3f01a64
Saurav Sharma <saurav@codenomad.net>
2024-04-11 22:05:18 +0530

    ISSUE FIXED

--- src/pages/Reseller-screens/Reseller/Dealer-Details/order.js
+++ src/pages/Reseller-screens/Reseller/Dealer-Details/order.js
@@ -1,19 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../../../../common/button";
 
 import ActiveIcon from "../../../../assets/images/icons/iconAction.svg";
-import arrowImage from "../../../../assets/images/dropdownArrow.png";
-import AddItem from "../../../../assets/images/icons/addItem.svg";
 import Search from "../../../../assets/images/icons/SearchIcon.svg";
+import download from "../../../../assets/images/download.png";
+import view from "../../../../assets/images/eye.png";
+import edit from "../../../../assets/images/edit-text.png";
+import remove from "../../../../assets/images/delete.png";
+import mark from "../../../../assets/images/pay.png";
+import process from "../../../../assets/images/return.png";
 import clearFilter from "../../../../assets/images/icons/Clear-Filter-Icon-White.svg";
-import Headbar from "../../../../common/headBar";
 import shorting from "../../../../assets/images/icons/shorting.svg";
 import Grid from "../../../../common/grid";
 import Input from "../../../../common/input";
 import DataTable from "react-data-table-component";
 import Select from "../../../../common/select";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { getOrderListByDealerId } from "../../../../services/dealerServices";
 import { getOrderListByResellerId } from "../../../../services/reSellerServices";
 import { getOrderListByCustomerId } from "../../../../services/customerServices";
@@ -25,9 +30,13 @@ import Primary from "../../../../assets/images/SetPrimary.png";
 import AddDealer from "../../../../assets/images/Disapproved.png";
 import {
   archiveOrders,
-  getOrders,
+  getContracts,
+  markPaid,
   processOrders,
 } from "../../../../services/orderServices";
+import PdfMake from "../../../pdfMakeOrder";
+import PdfGenerator from "../../../pdfViewer";
+import DocMakeOrderContainer from "../../../docMakeOrder";
 function OrderList(props) {
   console.log(props);
   const [selectedAction, setSelectedAction] = useState(null);
@@ -38,14 +47,18 @@ function OrderList(props) {
   const [processOrderErrors, setProcessOrderErrors] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [timer, setTimer] = useState(3);
+  const [message, setMessage] = useState("");
+  const [primaryMessage, setPrimaryMessage] = useState("");
+  const [secondaryMessage, setSecondaryMessage] = useState("");
+  const [contractDetails, setContractDetails] = useState();
   const [isDisapprovedOpen, setIsDisapprovedOpen] = useState(false);
   const [isArchiveOpen, setIsArchiveOpen] = useState(false);
   const [isModalOpen1, setIsModalOpen1] = useState(false);
   const navigate = useNavigate();
-
-  
+  const dropdownRef = useRef(null);
 
   const openArchive = (id) => {
+    setMessage("Would you like to Archive it?");
     SetOrderId(id);
     setIsArchiveOpen(true);
   };
@@ -58,21 +71,64 @@ function OrderList(props) {
     });
     setIsModalOpen(true);
   };
- 
+
+  const orderDetails = async (id) => {
+    const result = await getContracts(id);
+    console.log(result);
+    setContractDetails(result);
+    // setSelectedAction(null);
+  };
+
   const closeModal = () => {
     setIsModalOpen(false);
   };
+  const validationSchema = Yup.object().shape({});
 
+  const initialValues = {
+    orderId: "",
+    venderOrder: "",
+    dealerName: "",
+    resellerName: "",
+    customerName: "",
+    servicerName: "",
+    status: "",
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values) => {
+      getOrderList(values);
+
+      console.log(values);
+    },
+  });
+
+  const handleSelectChange = (name, selectedValue) => {
+    formik.setFieldValue(name, selectedValue);
+  };
   const closeModal1 = () => {
     setIsModalOpen1(false);
   };
   const openModal1 = () => {
-    console.log(orderId);
-    archiveOrders(orderId).then((res) => {
-      console.log(res);
-    });
-    setTimer(3);
-    setIsModalOpen1(true);
+    if (message == "Would you like to Archive it?") {
+      archiveOrders(orderId).then((res) => {
+        setPrimaryMessage("Archive Order Successfully");
+        setSecondaryMessage("You have successfully archive the order");
+        console.log(res);
+        setTimer(3);
+        setIsModalOpen1(true);
+      });
+    } else {
+      markPaid(orderId).then((res) => {
+        if (res.code == 200) {
+          setPrimaryMessage("Order Successfully Paid.");
+          setSecondaryMessage("You have successfully marked the order as paid");
+          setTimer(3);
+          setIsModalOpen1(true);
+        }
+      });
+    }
   };
 
   const closeArchive = () => {
@@ -82,8 +138,11 @@ function OrderList(props) {
     if (props.activeTab === "Orders" || props.activeTab === "Order") {
       getOrderList();
     }
-  }, [props]);
-
+  }, [props?.flag]);
+  const handleFilterIconClick = () => {
+    formik.resetForm();
+    getOrderList();
+  };
   useEffect(() => {
     let intervalId;
     if (isModalOpen && timer > 0) {
@@ -103,16 +162,16 @@ function OrderList(props) {
     }
     return () => clearInterval(intervalId);
   }, [isModalOpen, isModalOpen1, timer]);
-  
-  const getOrderList = async () => {
+
+  const getOrderList = async (data = {}) => {
     setLoading(true);
     let result = {};
     if (props.flag == "reseller") {
-      result = await getOrderListByResellerId(props.id);
+      result = await getOrderListByResellerId(props.id, data);
     } else if (props.flag == "customer") {
-      result = await getOrderListByCustomerId(props.id);
+      result = await getOrderListByCustomerId(props.id, data);
     } else if (props.flag == "dealer") {
-      result = await getOrderListByDealerId(props.id);
+      result = await getOrderListByDealerId(props.id, data);
     }
     setOrderList(result.result);
     setLoading(false);
@@ -127,6 +186,25 @@ function OrderList(props) {
   const calculateDropdownPosition = (index) => {
     const isCloseToBottom = orderList.length - index <= 10000;
     return isCloseToBottom ? "bottom-[1rem]" : "top-[1rem]";
+  };
+
+  const markasPaid = async (row) => {
+    setMessage(
+      `Would you prefer to make the full payment $ ${row.orderAmount} ?`
+    );
+    SetOrderId(row._id);
+    setIsArchiveOpen(true);
+  };
+
+  const formatOrderValue = (orderValue) => {
+    if (Math.abs(orderValue) >= 1e6) {
+      return (orderValue / 1e6).toFixed(2) + "M";
+    } else {
+      return orderValue.toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+    }
   };
 
   const columns = [
@@ -153,13 +231,18 @@ function OrderList(props) {
       sortable: true,
     },
     {
-      name: "# Of Contracts",
+      name: "# of Contracts",
       selector: (row) => row.noOfProducts,
       sortable: true,
     },
     {
       name: "Order Value",
-      selector: (row) => "$" + (row?.orderAmount ?? 0).toFixed(2),
+      selector: (row) =>
+        `$${
+          row?.orderAmount === undefined
+            ? parseInt(0).toLocaleString(2)
+            : formatOrderValue(row?.orderAmount ?? parseInt(0))
+        }`,
       sortable: true,
     },
     {
@@ -185,7 +268,12 @@ function OrderList(props) {
         // console.log(index, index % 10 == 9)
         return (
           <div className="relative">
-            <div onClick={() => setSelectedAction(row.unique_key)}>
+            <div
+              onClick={() => {
+                setSelectedAction(row.unique_key);
+                orderDetails(row._id);
+              }}
+            >
               <img
                 src={ActiveIcon}
                 className="cursor-pointer	w-[35px]"
@@ -194,34 +282,53 @@ function OrderList(props) {
             </div>
             {selectedAction === row.unique_key && (
               <div
-                className={`absolute z-[2] w-[120px] drop-shadow-5xl px-3 py-2 -right-3 mt-2 bg-white border rounded-lg shadow-md ${calculateDropdownPosition(
+                onClick={() => setSelectedAction(null)}
+                ref={dropdownRef}
+                className={`absolute z-[2] w-[130px] drop-shadow-5xl py-2 -right-3 mt-2 bg-white border rounded-lg shadow-md ${calculateDropdownPosition(
                   index
                 )}`}
               >
                 {row.status == "Pending" ? (
                   <>
                     <div
-                      className="text-center py-1 px-2 border-b cursor-pointer"
-                      onClick={() => navigate(`/editOrder/${row._id}`)}
+                      className="text-left py-1 px-2 flex border-b hover:font-semibold cursor-pointer"
+                      onClick={() => navigate(`/dealer/editOrder/${row._id}`)}
                     >
-                      Edit
+                      <img src={edit} className="w-4 h-4 mr-2" /> Edit
                     </div>
                     <div
-                      className="text-center py-1 px-2 border-b cursor-pointer"
+                      className="text-left py-1 px-2 flex border-b hover:font-semibold cursor-pointer"
                       onClick={() => openModal(row._id)}
                     >
-                      Process Order
+                      <img src={process} className="w-4 h-4 mr-2" /> Process
+                      Order
+                    </div>
+                    <div className="border-b">
+                      <PdfGenerator data={row._id}  setLoading={setLoading} />
                     </div>
                     <div
-                      className="text-center py-1 px-2 cursor-pointer"
+                      className="text-left py-1 px-2 flex cursor-pointer hover:font-semibold"
                       onClick={() => openArchive(row._id)}
                     >
-                      Archive
+                      <img src={remove} className="w-4 h-4 mr-2" /> Archive
                     </div>
                   </>
                 ) : (
-                    <Link to={`/orderDetails/${row._id}`} className="text-center py-1 px-2 cursor-pointer w-full flex justify-center">View</Link>
-                
+                  <>
+                    <Link
+                      to={`/dealer/orderDetails/${row._id}`}
+                      className="text-left py-1 px-2 cursor-pointer border-b hover:font-semibold w-full flex justify-start"
+                    >
+                      <img src={view} className="w-4 h-4 mr-2" /> View
+                    </Link>
+                    <div className="">
+                    <PdfGenerator
+                      data={row._id}
+                      setLoading={setLoading}
+                    />
+                    </div>
+                      {/* <DocMakeOrderContainer setLoading={setLoading} data={row._id} /> */}
+                  </>
                 )}
               </div>
             )}
@@ -237,9 +344,23 @@ function OrderList(props) {
     </div>
   );
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setSelectedAction(null);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   const status = [
-    { label: "Active", value: true },
-    { label: "Pending", value: false },
+    { label: "Active", value: "Active" },
+    { label: "Pending", value: "Pending" },
   ];
 
   return (
@@ -252,81 +373,97 @@ function OrderList(props) {
             </div>
             <div className="col-span-7">
               <div className="bg-[#F9F9F9] rounded-[30px] p-3 border-[1px] border-[#D1D1D1]">
-                <Grid className="!grid-cols-11">
-                  <div className="col-span-3 self-center">
-                    <Input
-                      name="Name"
-                      type="text"
-                      className="!text-[14px] !bg-[#f7f7f7]"
-                      className1="!text-[13px] !pt-1 placeholder-opacity-50 !pb-1 placeholder-[#1B1D21] !bg-[white]"
-                      label=""
-                      placeholder="ID"
-                    />
-                  </div>
-                  <div className="col-span-3 self-center">
-                    <Input
-                      name="Email"
-                      type="email"
-                      className="!text-[14px] !bg-[#f7f7f7]"
-                      className1="!text-[13px] !pt-1 placeholder-opacity-50 !pb-1 placeholder-[#1B1D21] !bg-[white]"
-                      label=""
-                      placeholder="Dealer Order no."
-                    />
-                  </div>
-                  <div className="col-span-3 self-center">
-                    <Select
-                      label=""
-                      options={status}
-                      OptionName='Status'
-                      color="text-[#1B1D21] opacity-50"
-                      className1="!pt-1 !pb-1 !text-[13px] !bg-[white]"
-                      className="!text-[14px] !bg-[#f7f7f7]"
-                    />
-                  </div>
-                  <div className="col-span-2 self-center flex justify-center">
-                    <Button type="submit" className="!p-0">
-                      <img
-                        src={Search}
-                        className="cursor-pointer "
-                        alt="Search"
+                <form onSubmit={formik.handleSubmit}>
+                  <Grid className="!grid-cols-7">
+                    <div className="col-span-2 self-center">
+                      <Input
+                        name="Name"
+                        type="text"
+                        className="!text-[14px] !bg-[#f7f7f7]"
+                        className1="!text-[13px] !pt-1 placeholder-opacity-50 !pb-1 placeholder-[#1B1D21] !bg-[white]"
+                        label=""
+                        placeholder="ID"
+                        {...formik.getFieldProps("orderId")}
                       />
-                    </Button>
-                    <Button type="submit" className="!bg-transparent !p-0">
-                      <img
-                        src={clearFilter}
-                        className="cursor-pointer	mx-auto"
-                        alt="clearFilter"
+                    </div>
+                    <div className="col-span-2 self-center">
+                      <Input
+                        name="orderNo"
+                        type="text"
+                        className="!text-[14px] !bg-[#f7f7f7]"
+                        className1="!text-[13px] !pt-1 placeholder-opacity-50 !pb-1 placeholder-[#1B1D21] !bg-[white]"
+                        label=""
+                        placeholder="Dealer P.O #"
+                        {...formik.getFieldProps("venderOrder")}
                       />
-                    </Button>
-                  </div>
-                </Grid>
+                    </div>
+                    <div className="col-span-2 self-center">
+                      <Select
+                        label=""
+                        options={status}
+                        OptionName="Status"
+                        color="text-[#1B1D21] opacity-50"
+                        className1="!pt-1 !pb-1 !text-[13px] !bg-[white]"
+                        className="!text-[14px] !bg-[#f7f7f7]"
+                        onChange={handleSelectChange}
+                        name="status"
+                        value={formik.values.status}
+                      />
+                    </div>
+
+                    <div className="col-span-1 self-center flex">
+                      <Button type="submit" className=" !bg-transparent !p-0">
+                        <img
+                          src={Search}
+                          className="cursor-pointer	"
+                          alt="Search"
+                        />
+                      </Button>
+
+                      <Button
+                        type="submit"
+                        className=" !bg-transparent !p-0"
+                        onClick={() => {
+                          handleFilterIconClick();
+                        }}
+                      >
+                        <img
+                          src={clearFilter}
+                          className="cursor-pointer	mx-auto"
+                          alt="clearFilter"
+                        />
+                      </Button>
+                    </div>
+                  </Grid>
+                </form>
               </div>
             </div>
           </Grid>
           <div className="mb-5 relative dealer-detail">
-          {loading ? (
-            <div className=" h-[400px] w-full flex py-5">
-            <div className="self-center mx-auto">
-              <RotateLoader color="#333" />
-            </div>
-          </div> ) : (
-            <DataTable
-              columns={columns}
-              data={orderList}
-              highlightOnHover
-              sortIcon={
-                <>
-                  {" "}
-                  <img src={shorting} className="ml-2" alt="shorting" />
-                </>
-              }
-              pagination
-              paginationPerPage={10}
-              noDataComponent={<CustomNoDataComponent />}
-              paginationComponentOptions={paginationOptions}
-              paginationRowsPerPageOptions={[10, 20, 50, 100]}
-            />
-          )}
+            {loading ? (
+              <div className=" h-[400px] w-full flex py-5">
+                <div className="self-center mx-auto">
+                  <RotateLoader color="#333" />
+                </div>
+              </div>
+            ) : (
+              <DataTable
+                columns={columns}
+                data={orderList}
+                highlightOnHover
+                sortIcon={
+                  <>
+                    {" "}
+                    <img src={shorting} className="ml-2" alt="shorting" />
+                  </>
+                }
+                pagination
+                paginationPerPage={10}
+                noDataComponent={<CustomNoDataComponent />}
+                paginationComponentOptions={paginationOptions}
+                paginationRowsPerPageOptions={[10, 20, 50, 100]}
+              />
+            )}
           </div>
         </div>
       </div>
@@ -355,10 +492,10 @@ function OrderList(props) {
         <div className="text-center py-3">
           <img src={Primary} alt="email Image" className="mx-auto my-4" />
           <p className="text-3xl mb-0 mt-2 font-[800] text-light-black">
-            Archive Order Successfully
+            {primaryMessage}
           </p>
           <p className="text-neutral-grey text-base font-medium mt-2">
-            You have successfully archive the order
+            {secondaryMessage}
           </p>
           <p className="text-neutral-grey text-base font-medium mt-2">
             Redirecting you on Order List Page {timer} seconds.
@@ -384,15 +521,9 @@ function OrderList(props) {
           </p>
 
           <p className="text-neutral-grey text-base font-medium mt-2">
-            {errorList &&
-              errorList.map((res) => {
-                console.log(res);
-                return (
-                  <p className="text-neutral-grey text-base font-medium mt-2">
-                    {res}
-                  </p>
-                );
-              })}
+            <p className="text-neutral-grey text-base font-medium mt-2">
+              {errorList}
+            </p>
           </p>
         </div>
       </Modal>
