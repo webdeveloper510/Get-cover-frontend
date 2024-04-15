81798dff151a65bd4cf728e779a16dce8a8a0d11
developer <developerweb6@gmail.com>
2024-04-16 02:58:31 +0530

    changes

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -42,60 +42,64 @@ function SidebarItem({
   onToggleExpand,
   onLinkClick,
   setExpandedItem,
+  isActiveValue,
+  sidebarItems
 }) {
   const hasItems = item.items && item.items.length > 0;
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(isActiveValue);
 
   const locationGet = useLocation();
+  
   useEffect(() => {
-    const CheckItemsActive = item?.items;
-    if (CheckItemsActive) {
-      CheckItemsActive.map((element, k) => {
-        if (locationGet.pathname === element.url) {
-          console.log(active);
-          setIsActive(active == element.url);
+  
+    if (isActiveValue === false) {
+      const CheckItemsActive = item?.items;
+  
+      if (CheckItemsActive) {
+        const childActive = CheckItemsActive.some(
+          (element) => active === element.url
+        );
+  
+        if (childActive) {
+          setExpandedItem(item.name);
         }
-      });
+      }
+      setIsActive(active === item.url || expandedItem === item.name);
+    } 
+    else {
+      const CheckItemsActive = item?.items;
+      if (CheckItemsActive) {
+        const childActive = CheckItemsActive.some(
+          (element) => active === element.url
+        );
+  
+        if (childActive) {
+          setExpandedItem(item.name);
+        }
+      }
+      setIsActive(active === item.url || expandedItem === item.name);
+  
     }
-    if (item.name === "Price Book") {
-      setIsActive(active === "Price Book" || expandedItem === "Price Book");
-      if (active === "Dealer" || expandedItem === "Dealer") {
-        setExpandedItem("Dealer");
+  }, [active, expandedItem, item, setExpandedItem, isActiveValue]);
+  
+  
+  
+  
+
+  const handleClick = () => {
+    if (hasItems) {
+      if (expandedItem === item.name) {
+        setExpandedItem(null);
+      } else {
+        onToggleExpand(item.name);
       }
-    } else if (item.name === "Claim") {
-      setIsActive(active === "Claim" || expandedItem === "Claim");
-      if (active === "Dealer" || expandedItem === "Dealer") {
-        setExpandedItem("Dealer");
-      }
-    } else if (item.name === "Dealer") {
-      setIsActive(active === "Dealer" || expandedItem === "Dealer");
-    } else if (item.name === "Servicer") {
-      setIsActive(active === "Servicer" || expandedItem === "Servicer");
-    } else if (item.name === "Customer") {
-      setIsActive(active === "Customer" || expandedItem === "Customer");
-    } else if (item.name === "Order") {
-      setIsActive(active === "Order" || expandedItem === "Order");
-    } else if (item.name === "Reporting") {
-      setIsActive(active === "Reporting" || expandedItem === "Reporting");
-    } else if (item.name === "Reseller") {
-      setIsActive(active === "Reseller" || expandedItem === "Reseller");
     } else {
-      // console.log("ok")
-      setIsActive(
-        active === item.url ||
-          active === item.name ||
-          expandedItem === item.name
-      );
+      setExpandedItem(null);
     }
-  }, [
-    active,
-    expandedItem,
-    item,
-    setExpandedItem,
-    locationGet.pathname,
-    setIsActive,
-  ]);
-  // console.log(isActive)
+  
+    onLinkClick(item.url);
+  };
+
 
   return (
     <li
@@ -108,14 +112,7 @@ function SidebarItem({
         className={`flex cursor-pointer d-flex ps-[20px] relative z-[2] mb-[3px] py-[19px] pe-3 ${
           isActive ? "bg-[#FFF] text-[#000] rounded-s-[30px]" : "text-light-grey"
         }`}
-        onClick={() => {
-          onLinkClick(item.url);
-          if (hasItems) {
-            onToggleExpand(item.name);
-          } else {
-            console.log(`Link to ${item.url} clicked`);
-          }
-        }}
+        onClick={handleClick}
       >
         {isActive ? (
           <img
@@ -238,11 +235,233 @@ function SidebarItem({
     </li>
   );
 }
-
+const Lists = [
+  {
+    name: "Dashboard",
+    url: "/dashboard",
+    image: DashboardImage,
+    active: ActiveDashboard,
+  },
+  {
+    name: "Order",
+    image: OrderImage,
+    active: ActiveOrder,
+    url: "/orderList",
+    items: [
+      {
+        name: "Order List",
+        url: "/orderList",
+        image: Dropdown1,
+        active: Actives,
+      },
+      {
+        name: "Add Order",
+        url: "/addOrder",
+        image: Dropdown2,
+        active: SeacondActive,
+      },
+      {
+        name: "Archive Order List",
+        url: "/archiveOrder",
+        image: Dropdown2,
+        active: ForthActive,
+      },
+    ],
+  },
+  {
+    name: "Contract",
+    url: "/contractList",
+    active: ActiveProduct,
+    image: ProductImage,
+  },
+  {
+    name: "Claim",
+    image: ClaimImage,
+    active: ActiveClaim,
+    url: "/claimList",
+    items: [
+      {
+        name: "Claim Listing",
+        url: "/claimList",
+        image: Dropdown1,
+        active: Actives,
+      },
+      {
+        name: "Add Claim",
+        url: "/addClaim",
+        image: Dropdown2,
+        active: SeacondActive,
+      },
+      {
+        name: "Add Bulk Claim",
+        url: "/addBulkClaim",
+        image: Dropdown2,
+        active: ForthActive,
+      },
+    ],
+  },
+  {
+    name: "Dealer",
+    image: DealerImage,
+    active: ActiveDealer,
+    url: "/dealerList",
+    items: [
+      {
+        name: "Dealer List",
+        url: "/dealerList",
+        image: Dropdown1,
+        active: Actives,
+      },
+      {
+        name: "Add Dealer",
+        url: "/addDealer",
+        image: Dropdown2,
+        active: SeacondActive,
+      },
+      {
+        name: "New Dealer Requests",
+        url: "/newDealerList",
+        image: Dropdown2,
+        active: ForthActive,
+      },
+    ],
+  },
+  {
+    name: "Reseller",
+    image: DealerImage,
+    active: ActiveDealer,
+    url: "/resellerList",
+    items: [
+      {
+        name: "Reseller List",
+        url: "/resellerList",
+        image: Dropdown1,
+        active: Actives,
+      },
+      {
+        name: "Add Reseller",
+        url: "/addReseller",
+        image: Dropdown2,
+        active: SeacondActive,
+      },
+    ],
+  },
+  {
+    name: "Customer",
+    image: CustomerImage,
+    active: ActiveCustomer,
+    url: "/customerList",
+    items: [
+      {
+        name: "Customer List",
+        url: "/customerList",
+        image: Dropdown1,
+        active: Actives,
+      },
+      {
+        name: "Add Customer",
+        url: "/addCustomer",
+        image: Dropdown2,
+        active: SeacondActive,
+      },
+    ],
+  },
+  {
+    name: "Servicer",
+    image: ServicerImage,
+    active: ActiveServicer,
+    url: "/servicerList",
+    items: [
+      {
+        name: "Servicer List",
+        url: "/servicerList",
+        image: Dropdown1,
+        active: Actives,
+      },
+      {
+        name: "Add Servicer",
+        url: "/addServicer",
+        image: Dropdown2,
+        active: SeacondActive,
+      },
+      {
+        name: "New Servicer Requests",
+        url: "/servicerRequestList",
+        image: Dropdown2,
+        active: ForthActive,
+      },
+    ],
+  },
+  {
+    name: "Reporting",
+    image: ReportImage,
+    active: ActiveReport,
+    url: "/sale",
+    items: [
+      {
+        name: "Sale",
+        url: "/sale",
+        image: Dropdown1,
+        active: Actives,
+      },
+      {
+        name: "Claims",
+        url: "/claims",
+        image: Dropdown2,
+        active: SeacondActive,
+      },
+      {
+        name: "Accounting",
+        url: "#",
+        image: Dropdown2,
+        active: ForthActive,
+      },
+    ],
+  },
+  {
+    name: "Price Book",
+    image: PriceImage,
+    active: ActivePriceBook,
+    url: "/dealerPriceList",
+    items: [
+      {
+        name: "Dealer Book",
+        url: "/dealerPriceList",
+        image: Dropdown1,
+        active: Actives,
+      },
+      {
+        name: "Company Price Book",
+        url: "/companyPriceBook",
+        image: Dropdown2,
+        active: SeacondActive,
+      },
+      {
+        name: "Upload Dealer Price Book",
+        url: "/uploadDealerBook",
+        image: Dropdown2,
+        active: ForthActive,
+      },
+      {
+        name: "Category",
+        url: "/category",
+        image: Dropdown2,
+        active: lastActive,
+      },
+    ],
+  },
+  {
+    name: "Manage Account",
+    url: "/manageAccount",
+    image: DashboardImage,
+    active: ActiveDashboard,
+  },
+];
 function SideBar() {
   const location = useLocation();
   const [active, setActive] = useState(location.pathname);
   const [expandedItem, setExpandedItem] = useState(active);
+  const [isActive, setIsActive] = useState(false);
   const [userType, setUserType] = useState(
     JSON.parse(localStorage.getItem("userDetails"))
   );
@@ -253,232 +472,17 @@ function SideBar() {
   };
 
   const handleToggleExpand = (itemName) => {
+     setIsActive(itemName)
     setExpandedItem((prevExpandedItem) =>
-      prevExpandedItem === itemName ? null : itemName
+     prevExpandedItem === itemName ? null : itemName
     );
-    console.log("Expanded Item:", itemName);
   };
 
   const handleLogOut = () => {
     localStorage.clear();
     navigate("/");
   };
-
-  console.log();
-  const Lists = [
-    {
-      name: "Dashboard",
-      url: "/dashboard",
-      image: DashboardImage,
-      active: ActiveDashboard,
-    },
-    {
-      name: "Order",
-      image: OrderImage,
-      active: ActiveOrder,
-      items: [
-        {
-          name: "Order List",
-          url: "/orderList",
-          image: Dropdown1,
-          active: Actives,
-        },
-        {
-          name: "Add Order",
-          url: "/addOrder",
-          image: Dropdown2,
-          active: SeacondActive,
-        },
-        {
-          name: "Archive Order List",
-          url: "/archiveOrder",
-          image: Dropdown2,
-          active: ForthActive,
-        },
-      ],
-    },
-    {
-      name: "Contract",
-      url: "/contractList",
-      active: ActiveProduct,
-      image: ProductImage,
-    },
-    {
-      name: "Claim",
-      image: ClaimImage,
-      active: ActiveClaim,
-      items: [
-        {
-          name: "Claim Listing",
-          url: "/claimList",
-          image: Dropdown1,
-          active: Actives,
-        },
-        {
-          name: "Add Claim",
-          url: "/addClaim",
-          image: Dropdown2,
-          active: SeacondActive,
-        },
-        {
-          name: "Add Bulk Claim",
-          url: "/addBulkClaim",
-          image: Dropdown2,
-          active: ForthActive,
-        },
-      ],
-    },
-    {
-      name: "Dealer",
-      image: DealerImage,
-      active: ActiveDealer,
-      items: [
-        {
-          name: "Dealer List",
-          url: "/dealerList",
-          image: Dropdown1,
-          active: Actives,
-        },
-        {
-          name: "Add Dealer",
-          url: "/addDealer",
-          image: Dropdown2,
-          active: SeacondActive,
-        },
-        {
-          name: "New Dealer Requests",
-          url: "/newDealerList",
-          image: Dropdown2,
-          active: ForthActive,
-        },
-      ],
-    },
-    {
-      name: "Reseller",
-      image: DealerImage,
-      active: ActiveDealer,
-      items: [
-        {
-          name: "Reseller List",
-          url: "/resellerList",
-          image: Dropdown1,
-          active: Actives,
-        },
-        {
-          name: "Add Reseller",
-          url: "/addReseller",
-          image: Dropdown2,
-          active: SeacondActive,
-        },
-      ],
-    },
-    {
-      name: "Customer",
-      image: CustomerImage,
-      active: ActiveCustomer,
-      items: [
-        {
-          name: "Customer List",
-          url: "/customerList",
-          image: Dropdown1,
-          active: Actives,
-        },
-        {
-          name: "Add Customer",
-          url: "/addCustomer",
-          image: Dropdown2,
-          active: SeacondActive,
-        },
-      ],
-    },
-    {
-      name: "Servicer",
-      image: ServicerImage,
-      active: ActiveServicer,
-      items: [
-        {
-          name: "Servicer List",
-          url: "/servicerList",
-          image: Dropdown1,
-          active: Actives,
-        },
-        {
-          name: "Add Servicer",
-          url: "/addServicer",
-          image: Dropdown2,
-          active: SeacondActive,
-        },
-        {
-          name: "New Servicer Requests",
-          url: "/servicerRequestList",
-          image: Dropdown2,
-          active: ForthActive,
-        },
-      ],
-    },
-    {
-      name: "Reporting",
-      image: ReportImage,
-      active: ActiveReport,
-      items: [
-        {
-          name: "Sale",
-          url: "/sale",
-          image: Dropdown1,
-          active: Actives,
-        },
-        {
-          name: "Claims",
-          url: "/claims",
-          image: Dropdown2,
-          active: SeacondActive,
-        },
-        {
-          name: "Accounting",
-          url: "#",
-          image: Dropdown2,
-          active: ForthActive,
-        },
-      ],
-    },
-    {
-      name: "Price Book",
-      image: PriceImage,
-      active: ActivePriceBook,
-      items: [
-        {
-          name: "Dealer Book",
-          url: "/dealerPriceList",
-          image: Dropdown1,
-          active: Actives,
-        },
-        {
-          name: "Company Price Book",
-          url: "/companyPriceBook",
-          image: Dropdown2,
-          active: SeacondActive,
-        },
-        {
-          name: "Upload Dealer Price Book",
-          url: "/uploadDealerBook",
-          image: Dropdown2,
-          active: ForthActive,
-        },
-        {
-          name: "Category",
-          url: "/category",
-          image: Dropdown2,
-          active: lastActive,
-        },
-      ],
-    },
-    {
-      name: "Manage Account",
-      url: "/manageAccount",
-      image: DashboardImage,
-      active: ActiveDashboard,
-    },
-  ];
+  
 
   const dealer = [
     {
@@ -821,6 +825,30 @@ function SideBar() {
     },
   ];
 
+  const sidebarItemsByRole = {
+    "Super Admin": Lists,
+    Dealer: dealer,
+    Reseller: reseller,
+    Customer: Customer,
+    Servicer:servicer
+  };
+
+  const sidebarItems = sidebarItemsByRole[userType?.role] || [];
+
+  const renderSidebarItems = sidebarItems.map((item, index) => (
+    <SidebarItem
+      key={index}
+      item={item}
+      active={active}
+      expandedItem={expandedItem}
+      onToggleExpand={handleToggleExpand}
+      onLinkClick={handleLinkClick}
+      setExpandedItem={setExpandedItem}
+      isActiveValue={isActive}
+      sidebarItems={sidebarItems}
+    />
+  ));
+
   return (
     <div className="xl:w-[220px] 2xl:w-[260px] min-h-[96vh] xl:h-full mb-8 fixed overflow-y-auto pl-3">
       <div className="bg-light-black min-h-[95vh] rounded-3xl relative pl-[5px]">
@@ -828,64 +856,8 @@ function SideBar() {
         <hr className=" border-[#474747] border-[1px]" />
         <div className="shadow-sm h-full ">
           <div className="mx-auto h-full mt-6">
-          <ul className="pb-5">
-              {userType?.role === "Super Admin"
-                ? Lists.map((bar, index) => (
-                    <SidebarItem
-                      key={index}
-                      item={bar}
-                      active={active}
-                      expandedItem={expandedItem}
-                      onToggleExpand={handleToggleExpand}
-                      onLinkClick={handleLinkClick}
-                      setExpandedItem={setExpandedItem}
-                    />
-                  ))
-                : userType?.role === "Servicer"
-                ? servicer.map((bar, index) => (
-                    <SidebarItem
-                      key={index}
-                      item={bar}
-                      active={active}
-                      expandedItem={expandedItem}
-                      onToggleExpand={handleToggleExpand}
-                      onLinkClick={handleLinkClick}
-                      setExpandedItem={setExpandedItem}
-                    />
-                  ))
-                : userType?.role === "Reseller" ? reseller.map((bar, index) => (
-                  <SidebarItem
-                    key={index}
-                    item={bar}
-                    active={active}
-                    expandedItem={expandedItem}
-                    onToggleExpand={handleToggleExpand}
-                    onLinkClick={handleLinkClick}
-                    setExpandedItem={setExpandedItem}
-                  />
-                ))
-                :  userType?.role === "Customer" ? Customer.map((bar, index) => (
-                  <SidebarItem
-                    key={index}
-                    item={bar}
-                    active={active}
-                    expandedItem={expandedItem}
-                    onToggleExpand={handleToggleExpand}
-                    onLinkClick={handleLinkClick}
-                    setExpandedItem={setExpandedItem}
-                  />
-                ))
-                : dealer.map((bar, index) => (
-                    <SidebarItem
-                      key={index}
-                      item={bar}
-                      active={active}
-                      expandedItem={expandedItem}
-                      onToggleExpand={handleToggleExpand}
-                      onLinkClick={handleLinkClick}
-                      setExpandedItem={setExpandedItem}
-                    />
-                  ) )}
+            <ul className="pb-5">
+              {renderSidebarItems}
               <li
                 className="cursor-pointer border-t-[#474747] mb-4 ps-[10px] rounded-s-[36px] border-t w-full text-[#fff]"
                 onClick={handleLogOut}
@@ -908,5 +880,4 @@ function SideBar() {
     </div>
   );
 }
-
 export default SideBar;
