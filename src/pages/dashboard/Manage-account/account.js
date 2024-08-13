e15cbc4bd05ef48a0ebe2f91b9136d32b705e3c8
Saurav Sharma <saurav@codenomad.net>
2024-08-13 11:54:31 +0530

    changes

--- src/pages/dashboard/Manage-account/account.js
+++ src/pages/dashboard/Manage-account/account.js
@@ -2113,7 +2113,7 @@ function Account() {
               <Grid className="!grid-cols-5 my-5  px-8">
                 <div className="col-span-2">
                   <Button
-                    className="border w-full !border-Bright-Grey !bg-[transparent] !text-light-black !text-sm !font-Regular"
+                    className="border w-full !border-Bright-Grey !bg-white !text-light-black !text-sm !font-Regular"
                     onClick={() => closeModal2()}
                   >
                     Cancel
