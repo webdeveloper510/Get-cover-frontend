c8d3d8e8a296a3f011526327fa8b1f2385037193
neek3190 <neek3190@gmail.com>
2024-08-22 17:51:03 +0530

    changes

--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -23,11 +23,6 @@ const PrivateRoute = ({ element, path, withoutLogin, role }) => {
         navigate(path, { replace: true });
       }
 
-      console.log(
-        userToken?.role,
-        role,
-        userToken?.role !== role.charAt(0).toUpperCase() + role.slice(1)
-      );
       if (
         role &&
         userToken?.role != undefined &&
